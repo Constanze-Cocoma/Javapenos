53609e07ffbfdcd6c1e6d75c6605ee25253b1285
Constanze Juergens <constanzejuergens@213.107.62.94.rev.vodafone.pt>
2019-02-23 15:31:07 +0000

    added class Counter to count total points and javapenos left

---
+++ src/org/academiadecodigo/bootcamp/javapenos_game/Counter.java
@@ -0,0 +1,36 @@
+package src.org.academiadecodigo.bootcamp.javapenos_game;
+
+public class Counter {
+
+    private int totalPoints;
+    private int javapenosLeft;
+
+
+    public Counter(int javapenosLeft) {
+        totalPoints = 0;
+        this.javapenosLeft = javapenosLeft;
+    }
+
+
+
+
+    public int getPoints() {
+        return totalPoints;
+    }
+
+    public void setPoints(int points) {
+        totalPoints = totalPoints + points;
+    }
+
+    public int getJavapenosLeft() {
+        return javapenosLeft;
+    }
+
+    public void setJavapenosLeft(int javapenosGone) {
+        javapenosLeft = javapenosLeft - javapenosGone;
+    }
+
+
+
+
+}
