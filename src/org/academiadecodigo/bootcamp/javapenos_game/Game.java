bd7b3c42c93c0e860a71834e1d31f550bfcb13fa
Constanze Juergens <constanzejuergens@213.107.62.94.rev.vodafone.pt>
2019-02-24 10:33:46 +0000

    worked on counting methods for score and javapenos left

--- src/org/academiadecodigo/bootcamp/javapenos_game/Game.java
+++ src/org/academiadecodigo/bootcamp/javapenos_game/Game.java
@@ -2,42 +2,77 @@ package src.org.academiadecodigo.bootcamp.javapenos_game;
 
 public class Game {
 
-    private Grid startGrid;
-    private Grid gameGrid;
+    private StartGrid startGrid;
+    private GameGrid gameGrid;
+    private EndGrid endGrid;
     private Javapeno[] javapenos;
-    private int delay;
+    private Counter counter;
+    private int delay; // o game ainda precisa dume referência ao delay?
     private int totalJavapenos;
     private int totalPoints;
 
     public Game(int totalJavapenos) {
         startGrid = new StartGrid(10, 576, 768);
-        delay = 50;
+        delay = 50; //where is delay used???
+        this.totalJavapenos = totalJavapenos;
         ((StartGrid) startGrid).startInit();
     }
 
     public void init() throws InterruptedException {
         gameGrid = new GameGrid(10, 576, 768);
         javapenos = new Javapeno[totalJavapenos];
+        counter = new Counter(totalJavapenos);
         for (int i = 0; i < totalJavapenos; i++) {
             javapenos[i] = JavapenoFactory.getNewJavapeno();
-            //Thread.sleep(500); //falta try catch
-            start();
-
+            counter.subtractJavapenosLeft();
+            try {
+                Thread.sleep(500); //falta try catch
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        start();
     }
 
-    public void start() throws InterruptedException {
-        while (true) {
+    private void start() throws InterruptedException {
+        while (counter.getJavapenosLeft() > 0) {
             Thread.sleep(500);
             moveJavapenos();
+            //call method to count javapenos caught (collision detector)
+            //collisionDetector.
+            //add if-statement to check for javapenos left and call end() if 0 javapenos left
         }
+        //if (counter.getJavapenosLeft() == 0 && //) {
+        end();
     }
 
+
     public void moveJavapenos() {
-        for (Javapeno j: javapenos) {
+        for (Javapeno j : javapenos) {
             j.move();
         }
     }
 
+    public void end() {
+        // print total score to screen
+        // go back to start grid
+    }
+
+    public void countScore() {
+        //Collision Detector - check for collisions with player
+        //if(collision with green javapeno) {
+        // counter.addPoint();
+        // if (collision with red javapeno) {
+        // counter.subtractPoint();
+    }
+
+
+    public void countJavapenos() {
+    //collision detector - check for collisions with player and floor
+        //counter.subtractJavapenosLeft();
+    }
+
 
 }
+
+
